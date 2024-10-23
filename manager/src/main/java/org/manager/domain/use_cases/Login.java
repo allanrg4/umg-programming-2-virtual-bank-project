4efb934d2e367374cc2fb917eec0d24355995d0f
Allan Rodriguez <0x.allan@gmail.com>
2024-10-22 22:03:07 -0600

    feat: user not found exception

--- manager/src/main/java/org/manager/domain/use_cases/Login.java
+++ manager/src/main/java/org/manager/domain/use_cases/Login.java
@@ -2,6 +2,7 @@ package org.manager.domain.use_cases;
 
 import org.core.domain.UseCase;
 import org.manager.domain.entities.User;
+import org.manager.domain.exceptions.UserNotFoundException;
 import org.manager.domain.params.LoginParams;
 import org.manager.domain.repositories.AuthRepository;
 
@@ -17,6 +18,10 @@ public class Login implements UseCase<User, LoginParams> {
             throw new IllegalArgumentException("Username and password must be provided");
         }
 
-        return authRepo.login(params.username(), params.password());
+        try {
+            return authRepo.login(params.username(), params.password());
+        } catch (UserNotFoundException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
